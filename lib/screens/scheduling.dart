a0dfee11d55f46ab93ec56084ef48db2ad0b75fb
Mahlet <m.tilahun@alustudent.com>
2026-02-04 22:58:05 +0300

    Create scheduling screen

---
+++ lib/screens/scheduling.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+/// Scheduling module for scheduling new academic sessions,
+/// viewing weekly schedule, recording attendance,
+/// removing scheduled sessions and modifying session details.
