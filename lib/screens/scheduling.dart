6f1f2bf5f2ac728b39213898f126618364ee3b22
mahlet-tilahun <m.tilahun@alustudent.com>
2026-02-05 20:27:11 +0300

    Initialize Flutter Project

--- lib/screens/scheduling.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// Scheduling module for scheduling new academic sessions,
-/// viewing weekly schedule, recording attendance,
-/// removing scheduled sessions and modifying session details.
