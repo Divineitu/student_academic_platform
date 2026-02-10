f466a4c60bb1c35fc778bebc2510cfd82c252288
Julie Isaro <j.isaro@alustudent.com>
2026-02-10 12:09:58 +0100

    Add CustomTextField widget for reusable text input

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+// A custom text field widget that can be reused across the app.
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  final IconData icon;
+  final bool obscureText;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.label,
+    required this.icon,
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      style: const TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: const TextStyle(color: Colors.white70),
+        prefixIcon: Icon(icon, color: const Color(0xFFFFB800)),
+        filled: true,
+        fillColor: Colors.white.withOpacity(0.1),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
